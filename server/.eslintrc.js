27b2f66f4eaf3620e2a522b46cac217001d14e7e
d1r0l <xd1r0lx@gmail.com>
2024-08-13 15:08:27 +0300

    style: :rotating_light: updated server lint config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0
-  }
+  },
+  ignorePatterns: ['**/build/**/*.js']
 }
