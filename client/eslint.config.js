c3f505f2815353e767b4ce84b5741c8216109aec
d1r0l <xd1r0lx@gmail.com>
2024-09-09 20:24:35 +0300

    feat: :arrow_up: updated dependancies
    migratet to flat eslint config, updated cypress config

---
+++ client/eslint.config.js
@@ -0,0 +1,58 @@
+const eslint = require('@eslint/js')
+const globals = require('globals')
+const react = require('eslint-plugin-react')
+const reactRedux = require('eslint-plugin-react-redux')
+const jest = require('eslint-plugin-jest')
+const prettier = require('eslint-plugin-prettier')
+const cypress = require('eslint-plugin-cypress/flat')
+
+module.exports = [
+  eslint.configs.recommended,
+  react.configs.flat.recommended,
+  cypress.configs.recommended,
+  {
+    ...jest.configs['flat/recommended'],
+    files: ['**/*.js'],
+    plugins: {
+      react,
+      'react-redux': reactRedux,
+      jest,
+      cypress,
+      prettier
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    rules: {
+      'linebreak-style': ['error', 'unix'],
+      'semi': ['error', 'never'],
+      'react/react-in-jsx-scope': 0,
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+    },
+    settings: {
+      react: { version: 'detect' }
+    }
+  },
+  {
+    files: ['**/*.config.js'],
+    languageOptions: {
+      sourceType: 'commonjs'
+    }
+  },
+  {
+    ignores: ['**/build/**/*.js']
+  }
+]
